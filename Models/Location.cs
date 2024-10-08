7e30fbe34456a9072c06775001a2e8ece991966e
khaled khamees <khaledkhameesabdelaleem@gmail.com>
2024-10-08 21:07:12 +0300

    Add project files.

---
+++ Models/Location.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DeliveryTrackingAPI.Models
+{
+    public class Location
+    {
+        public int Id { get; set; }
+        public int DeliveryStaffId { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        public DateTime Timestamp { get; set; }
+        [ForeignKey(nameof(DeliveryStaffId))]
+        public DeliveryStaff DeliveryStaff { get; set; }
+    }
+}
