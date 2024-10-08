c3856d9a518b41fecf6e68568cb6c5dd51682c7f
khaled khamees <khaledkhameesabdelaleem@gmail.com>
2024-10-09 00:06:06 +0300

    Adding the base of controllers

---
+++ Controllers/DeliveryStaffController.cs
@@ -0,0 +1,36 @@
+﻿using DeliveryTrackingAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DeliveryTrackingAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DeliveryStaffController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public DeliveryStaffController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/DeliveryStaff
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<DeliveryStaff>>> GetDeliveryStaff()
+        {
+            return await _context.DeliveryStaff.ToListAsync();
+        }
+
+        // POST: api/DeliveryStaff
+        [HttpPost]
+        public async Task<ActionResult<DeliveryStaff>> PostDeliveryStaff(DeliveryStaff deliveryStaff)
+        {
+            _context.DeliveryStaff.Add(deliveryStaff);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetDeliveryStaff", new { id = deliveryStaff.Id }, deliveryStaff);
+        }
+    }
+}
---
+++ Controllers/LocationController.cs
@@ -0,0 +1,39 @@
+﻿using DeliveryTrackingAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DeliveryTrackingAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LocationController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public LocationController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Location/{staffId}
+        [HttpGet("{staffId}")]
+        public async Task<ActionResult<IEnumerable<Location>>> GetStaffLocation(int staffId)
+        {
+            return await _context.Locations
+                .Where(l => l.DeliveryStaffId == staffId)
+                .OrderByDescending(l => l.Timestamp)
+                .ToListAsync();
+        }
+
+        // POST: api/Location
+        [HttpPost]
+        public async Task<ActionResult<Location>> PostLocation(Location location)
+        {
+            _context.Locations.Add(location);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetStaffLocation", new { staffId = location.DeliveryStaffId }, location);
+        }
+    }
+}
