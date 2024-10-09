9a8ecb128801fe77641acdd3861403ba094bc36d
khaled khamees <khaledkhameesabdelaleem@gmail.com>
2024-10-09 06:31:54 +0300

    Add Update and Delete endpoints on DeliveryStaffController

--- Controllers/DeliveryStaffController.cs
+++ Controllers/DeliveryStaffController.cs
@@ -32,5 +32,36 @@ namespace DeliveryTrackingAPI.Controllers
 
             return CreatedAtAction("GetDeliveryStaff", new { id = deliveryStaff.Id }, deliveryStaff);
         }
+        // Delete : api/DeliveryStaff
+        [HttpDelete]
+        public async Task<ActionResult<DeliveryStaff>> DeleteDeliveryStaff(int id)
+        {
+            var delivaryFromDb = _context.DeliveryStaff.FirstOrDefault(x => x.Id == id);
+            if (id <= 0 || delivaryFromDb == null)
+            {
+                return BadRequest();
+            }
+            _context.DeliveryStaff.Remove(delivaryFromDb);
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+        // Update : api/DeliveryStaff
+        [HttpPatch]
+        public async Task<ActionResult<DeliveryStaff>> UpdateDeliveryStaff(int id, DeliveryStaff updatedStaff)
+        {
+            var delivaryFromDb = await _context.DeliveryStaff.FindAsync(id);
+            if (delivaryFromDb == null)
+            {
+                return NotFound();
+            }
+
+            delivaryFromDb.Name = updatedStaff.Name ?? delivaryFromDb.Name;
+            delivaryFromDb.PhoneNumber = updatedStaff.PhoneNumber ?? delivaryFromDb.PhoneNumber;
+
+            _context.DeliveryStaff.Update(delivaryFromDb);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
     }
 }
